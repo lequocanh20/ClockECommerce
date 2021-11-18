e3abcad33eeb0076a80c898955861fffcbf8f5fc
Lê Quốc Anh <66353897+lequocanh20@users.noreply.github.com>
2021-11-19 00:25:33 +0700

    update parameter detail in product

--- clockECommerce.AdminApp/Controllers/ProductController.cs
+++ clockECommerce.AdminApp/Controllers/ProductController.cs
@@ -169,6 +169,7 @@ namespace clockECommerce.AdminApp.Controllers
 
             var detailVm = new ProductViewModel()
             {
+                originPrice = product.originPrice,
                 Price = product.Price,
                 Stock = product.Stock,
                 Name = product.Name,
